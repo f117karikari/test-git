389b73b18e8586c83ff809df1dc85caed633ab53
f117karikari <f117karikari@gmail.com>
2014-07-02 19:41:01 +0900

    レビュー後修正

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -2,8 +2,9 @@ package main;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println(FinalString.hello);
 		System.out.println(FinalString.goodBye);
+		System.out.println(FinalString.hello);
+		
 		System.out.println(FinalString.helloGoodBye);		
 	}
 }
