7e938f0304d31243c0237921560050321f527059
f117karikari <f117karikari@gmail.com>
2014-07-08 11:34:10 +0900

    #6 日付追加

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -1,9 +1,12 @@
 package main;
 
+import java.util.Date;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println(FinalString.goodBye);
 		System.out.println(FinalString.hello);
 		System.out.println(FinalString.helloGoodBye);
+		System.out.println(new Date());		
 	}
 }
