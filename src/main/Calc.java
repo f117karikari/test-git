ec5eb90db666dc4870167798b5d4fc8c19ee4699
f117karikari <f117karikari@gmail.com>
2014-07-10 15:18:13 +0900

    足し算引き算追加

--- src/main/Calc.java
+++ src/main/Calc.java
@@ -1,6 +1,14 @@
 package main;
 public class Calc {
 	
+	public int tashi(int x,int y){
+		return x + y;
+	}
+	
+	public int hiki(int x,int y){
+		return x - y;
+	}
+	
 	public int kake(int x,int y){
 		return x * y;
 	}
