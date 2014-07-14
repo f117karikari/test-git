75870ed76a742d1d355b400166fd5784014c7754
f117karikari <f117karikari@gmail.com>
2014-07-14 17:58:34 +0900

    コンフリクト解消

--- src/main/Calc.java
+++ src/main/Calc.java
@@ -1,28 +1,26 @@
 package main;
 public class Calc {
 	
-	public int tashi(int x,int y){
-		return x + y;
+	public String kakezan(int x, int y){
+		return kake(x,y).toString();
 	}
-	
-	public int hiki(int x,int y){
-		return x - y;
-	}
-	
-	public int kake(int x,int y){
-		return x * y;
-	}
-
-	public int wari(int x,int y){
+	public String warizan(int x,int y){
 		// 整数しか返せない
 		if(x < y){
-			return 0;			
+			return "結果が少数になる割り算はできません。";			
 		}
 		// 数学上は正しくないが0を返す
 		if(y == 0 ){
-			return 0;
-		}
-		return x / y;
+			return "0除算はできません。";
+		}		
+		return wari(x,y).toString();
 	}
 	
+	private Integer kake(int x,int y){
+		return x * y;
+	}
+
+	private Integer wari(int x,int y){
+		return x / y;
+	}
 }
