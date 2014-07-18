39f048968fbe3a1242bdcb0ad03520afa36a9d82
f117karikari <f117karikari@gmail.com>
2014-07-18 15:51:32 +0900

    #test3

--- src/main/Consts.java
+++ src/main/Consts.java
@@ -5,5 +5,6 @@ public class Consts {
 	public static String hello = "Hello World !!";
 	public static String goodBye= "Good Bye Japan !!";
 	public static String helloGoodBye= "Hello,Good Bye ...";
+	public static String testGti= "test-git.git";
 	public static String test= "test";
 }
