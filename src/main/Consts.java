e765266a282a8cfbe09146d437298049e8435b89
f117karikari <f117karikari@gmail.com>
2014-07-18 15:43:25 +0900

    #test2

--- src/main/Consts.java
+++ src/main/Consts.java
@@ -5,4 +5,5 @@ public class Consts {
 	public static String hello = "Hello World !!";
 	public static String goodBye= "Good Bye Japan !!";
 	public static String helloGoodBye= "Hello,Good Bye ...";
+	public static String test= "test";
 }
