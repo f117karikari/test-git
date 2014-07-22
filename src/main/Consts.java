258d7f97bc0ae9380b966ec266010862f623a4cd
f117karikari <f117karikari@gmail.com>
2014-07-22 15:23:34 +0900

    typo

--- src/main/Consts.java
+++ src/main/Consts.java
@@ -5,6 +5,6 @@ public class Consts {
 	public static String hello = "Hello World !!";
 	public static String goodBye= "Good Bye Japan !!";
 	public static String helloGoodBye= "Hello,Good Bye ...";
-	public static String testGti= "test-git.git";
+	public static String testGit= "test-git.git";
 	public static String test4= "test4";
 }
