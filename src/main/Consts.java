59833b52ebe78c319f29c29fcf95f25a8d3b3b2b
f117karikari <f117karikari@gmail.com>
2014-08-04 16:27:35 +0900

    test is bad

--- src/main/Consts.java
+++ src/main/Consts.java
@@ -6,4 +6,5 @@ public class Consts {
 	public static String goodBye= "Good Bye Japan !!";
 	public static String helloGoodBye= "Hello,Good Bye ...";
 	public static String testGit= "test is good.";
+	public static String testWebHook= "test is bad.";
 }
