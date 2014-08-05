fe3c4fdcfd7d0064f41d8205ee4203dab0f65d3f
f117karikari <f117karikari@gmail.com>
2014-08-05 17:58:08 +0900

    Y8743 要件連携テスト

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -3,11 +3,18 @@ package main;
 import java.util.Date;
 
 public class HelloWorld {
+
+	/**
+	 * メインクラス
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println(Consts.goodBye);
 		System.out.println(Consts.hello);
 		System.out.println(Consts.helloGoodBye);
 		System.out.println(Consts.testGit);
 		System.out.println(new Date());	
+		
+		System.exit(0);
 	}
 }
