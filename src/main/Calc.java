f5b97bf85c96b58bb276e41571a210ec10f19d02
igaiga0540 <igaiga0540@users.noreply.github.com>
2014-08-27 14:15:08 +0900

    Update Calc.java

--- src/main/Calc.java
+++ src/main/Calc.java
@@ -1,17 +1,21 @@
 package main;
 public class Calc {
+	// 足し算
 	public int tashi(int x,int y){
 		return x + y;
 	}
 	
+	// 引き算
 	public int hiki(int x,int y){
 		return x + y;
 	}
 	
+	// 掛け算
 	public int kake(int x,int y){
 		return x * y;
 	}
 
+	// 割り算
 	public int wari(int x,int y){
 		// 整数しか返せない
 		if(x < y){
